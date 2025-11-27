682a3d175de767990e42e2f68d14cc0bde189961
armannik13 <alevtina9876123@gmail.com>
2025-11-27 13:27:34 +0300

    Update logic.py

--- cyan/logic.py
+++ cyan/logic.py
@@ -55,7 +55,7 @@ def main(parser: ArgumentParser) -> None:
       app.remove_encrypted_extensions()
 
     if args.f is not None:
-      app.executable.inject(args.f, tmpdir, args.inject_to_path, args.z)
+      app.executable.inject(args.f, tmpdir, args.inject_to_path, args.j)
     if args.n is not None:
       app.plist.change_name(args.n)
     if args.v is not None:
