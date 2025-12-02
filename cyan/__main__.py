b349af17c088b6c7e4ced89ea9646a097406d4bf
armannik13 <alevtina9876123@gmail.com>
2025-12-02 15:09:58 +0300

    Update __main__.py

--- cyan/__main__.py
+++ cyan/__main__.py
@@ -97,7 +97,7 @@ def main() -> None:
     help="use custom tweaks path"
   )
   parser.add_argument(
-    "-r", "--patch-plugins", nargs="?", const=True, default=False, metavar="dylib",
+    "-r", "--patch-plugins", nargs="?", const=True, default=False, metavar="custom dylib",
     help="patch plugins"
   )
 
